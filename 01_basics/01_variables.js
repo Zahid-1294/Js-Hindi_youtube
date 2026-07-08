1c8768da3c0a94603a7dd856e52ba4b75f23f1b3
Zahid-1294 <shaikhzahid.1506@gmail.com>
2026-07-09 00:07:56 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,11 @@ var accountpassword = "1234"
 accountcity = "Thane"
 
 
+/* prefer not to use var
+coz gives issue in block scope and functional scope
+*/
+
+
 // const use kiye matlb variable ki value change nahi hogi //
 
 
