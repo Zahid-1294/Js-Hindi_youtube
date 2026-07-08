1ad0d05f2f6d7628a9e29ce247895b2c32e61a7d
Zahid-1294 <shaikhzahid.1506@gmail.com>
2026-07-08 18:22:41 +0000

    lesson 1 done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = "1234"
+let accountemail = "shaikhzah@.com"
+var accountpassword = "1234"
+accountcity = "Thane"
+
+
+// const use kiye matlb variable ki value change nahi hogi //
+
+
+console.table([accountId, accountemail, accountpassword, accountcity])
