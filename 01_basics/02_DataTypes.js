ce6e6010471cd524818dc89b1e15ae677ab0844a
Zahid-1294 <shaikhzahid.1506@gmail.com>
2026-07-10 12:44:08 +0000

    datatypes

--- 01_basics/02_DataTypes.js
+++ 01_basics/02_DataTypes.js
@@ -1,3 +1,6 @@
+"use strict ";// treat all js code as newer version
+
+
 console.log(3 + 3 )
 console.log("zahid")
 
@@ -7,3 +10,23 @@ console.log(2 +
 
 // the above code looks very bad
 // So always remember code reliability should be high
+
+let name = "zahid" // this string datatype //
+let age = 19 // number datataype // 
+let IsLoggedIn = false/true // boolean datatype //
+
+// various datatypes //
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
